ab713347b3e723929e4fcf8999c2a63ccebdc4b0
emrassel <emrassel@umich.edu>
2019-02-17 18:08:22 -0500

    Bare bones for suit information classes

---
+++ CLAWS/Scripts/SuitInformation/BarData.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace suitInfo
+{
+    interface DisplayData
+    {
+        void Display();
+    }
+    public class BarData : DisplayData
+    {
+        public enum Color {
+            Red, Yellow, Green
+        }
+
+        int value;
+        int start;
+        float percentage;
+        Color color;
+        string title;
+
+        public BarData() { }
+        public BarData(int val, int per, string titl)
+        {
+            start = val; value = val;
+            percentage = (float)value / (float)start;
+            title = titl;
+            Update_color(percentage);
+        }
+        public void change_val(int val)
+        {
+            value = val;
+            percentage = (float)value / (float)start;
+            Update_color(percentage);
+        }
+        public void Update_color(float percent)
+        {
+            if (percent >= 50) color = Color.Green;
+            else if (percent < 50 && percent >= 30) color = Color.Yellow;
+            else color = Color.Red;
+        }
+
+        public void DisplayData.Display()
+        {
+            // add code to display each bar
+
+        }
+    }
+    public class Simple
+    {
+        const int NUM_DATA = 11;
+        readonly string[] units;
+
+        public Simple(){
+            units = { "mL","seconds",...};
+        }
+        public void change_unit(int index, string unit)
+        {
+            units[index] = unit;
+        }
+
+        // this function gets called when
+        public void Display(Dictionary<string, int> data)
+        {
+            // add code to display simple text in Unity
+                // takes in dictionary and prints each value on screen
+
+        }
+    }
+}
---
+++ CLAWS/Scripts/SuitInformation/SuitInformation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+namespace suitInfo
+{
+    class SuitInformation
+    {
+        const int NUM_DATA = 11;
+        Dictionary<string, int> data = new Dictionary<string, int>(NUM_DATA);
+
+        BarData oxygen = new BarData(0, 100, "oxygen");
+        BarData battery_life = new BarData(0, 100, "battery life");
+        BarData water = new BarData(0, 100, "water");
+        //Simple simple_data;
+
+        public void update_suitInformation()
+        {
+            // read in data using UnityWebRequest
+            UnityWebRequest www = UnityWebRequest.Get("http://ip_of_the_server/api/telemetry/recent");
+            //yeild return www.SendWebRequest();
+
+            //if (www.isNetworkError || www.isHttpError)
+            if(www.isHttpError) Debug.Log(www.error);
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+
+                // Or retrieve results as binary data
+                byte[] results = www.downloadHandler.data;
+
+                // retrieve as JSON file
+
+                // store JSON information into dictionary
+                data["p_sub"] = 3; // from JSON file
+
+            
+            }
+
+            // call functions to display data
+            oxygen.Display(); // two more
+            battery_life.Display();
+            water.Display();
+        }
+    }
+}
