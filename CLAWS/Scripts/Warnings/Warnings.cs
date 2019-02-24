c78152013b3f522ce3c6473249afee778f7f9c37
roseva <roseva@umich.edu>
2019-02-24 13:44:39 -0500

    initial commit

---
+++ CLAWS/Scripts/Warnings/Warnings.cs
@@ -0,0 +1,23 @@
+using HoloToolkit.Unity;
+using UnityEngine;
+
+namespace Warnings
+{
+  public class TagalongAction : InteractableAction
+  {
+    private GameObject warning;
+
+    private void Awake()
+    {
+      warning = Instantiate(warning);
+      warning.SetActive(true);
+
+      Billboard billboard = warning.AddComponent<Billboard>();
+      warning.AddComponent<SimpleTagalong>();
+    }
+
+
+  }
+
+
+}
