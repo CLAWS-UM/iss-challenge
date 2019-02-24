ca5b5db398f10d6f1f359703d36fc1b00d3b3460
Rosie <roseva@umich.edu>
2019-02-24 18:07:48 -0500

    add warning types

--- CLAWS/Scripts/Warnings/Warnings.cs
+++ CLAWS/Scripts/Warnings/Warnings.cs
@@ -1,6 +1,14 @@
 using HoloToolkit.Unity;
 using UnityEngine;
 
+public enum warningLevel
+{
+   Low = 0,
+   Medium,
+   Urgent
+}
+
+
 namespace Warnings
 {
   public class TagalongAction : InteractableAction
