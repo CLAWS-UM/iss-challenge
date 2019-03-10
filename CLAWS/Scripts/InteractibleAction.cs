260042769f02f7e3e0a11477501f52d78446ebf9
rupaln <rupaln@umich.edu>
2019-03-09 20:48:44 -0500

    add interactible action script

---
+++ CLAWS/Scripts/InteractibleAction.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace CLAWS
+{
+    /// <summary>
+    /// InteractibleAction performs custom actions when you tap on the holograms.
+    /// </summary>
+    public abstract class InteractibleAction : MonoBehaviour
+    {
+        public abstract void PerformAction();
+    }
+}
