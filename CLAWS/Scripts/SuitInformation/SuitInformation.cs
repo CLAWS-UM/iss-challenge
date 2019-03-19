65033ad667086db694310d89426d9facac6ca696
emrassel <emrassel@umich.edu>
2019-03-19 12:54:23 -0400

    Update SuitInformation.cs

--- CLAWS/Scripts/SuitInformation/SuitInformation.cs
+++ CLAWS/Scripts/SuitInformation/SuitInformation.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using UnityEngine;
 using System.Collections;
-using UnityEngine.Networking;
+using UnityEngine.Networking; // web request
 
 namespace suitInfo
 {
