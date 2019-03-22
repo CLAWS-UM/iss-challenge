47d9042e628f30b49b4ca0851e537a3e41ae56ee
Sahil <Sahilf@umich.edu>
2019-03-22 03:49:55 -0400

    Added base UI stuffs

---
+++ CLAWS/Assets/Scripts/Communications.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Communications : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera camera = Camera.main;
+        Vector3 centre = camera.ViewportToWorldPoint(new Vector3((float)0.5, 0, (float)20));
+        Vector3 offset = new Vector3(0, (GetComponent<Renderer>().bounds.size.y / 2), 0);
+        transform.position = centre + offset;
+    }
+}
---
+++ CLAWS/Assets/Scripts/HeartRate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartRate : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera camera = Camera.main;
+        Vector3 centre = camera.ViewportToWorldPoint(new Vector3(1, 0, (float)20));
+        Vector3 offset = new Vector3(-(GetComponent<Renderer>().bounds.size.x / 2), (GetComponent<Renderer>().bounds.size.y / 2), 0);
+        transform.position = centre + offset;
+    }
+}
