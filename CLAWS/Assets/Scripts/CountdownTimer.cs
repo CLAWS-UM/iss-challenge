5b555297405ed8ad4466f310046a002dbec986ad
Emily Rassel <emrassel@umich.edu>
2019-04-07 00:19:14 -0400

    Clock integration

---
+++ CLAWS/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    float currentTime;
+    float startTime = 0f;
+
+    //[SerializeField] Text timer;
+
+    void Start()
+    {
+        currentTime = startTime;
+        //timer.text = currentTime.ToString("0.00");
+    
+    }
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        //timer.text = currentTime.ToString("0.00");
+    }
+
+    void OnGUI()
+    {
+        float hours = Mathf.Floor(currentTime / 3600);
+        float minutes = Mathf.Floor((currentTime % 3600) / 60);
+        float seconds = currentTime % 60;
+
+        GUIStyle clockStyle = new GUIStyle();
+        clockStyle.fontSize = 16;
+        clockStyle.normal.textColor = Color.white;
+        GUI.Label(new Rect(900, 3, 250, 100), hours + ":" + minutes + ":" + Mathf.RoundToInt(seconds), clockStyle);
+    }
+}
