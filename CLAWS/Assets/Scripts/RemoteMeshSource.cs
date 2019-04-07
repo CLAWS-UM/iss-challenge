6a167c875779044145b3fd4f4104d5547fa0b85c
cesarmu <cesarmu@umich.edu>
2019-04-06 23:08:49 -0400

    p

--- CLAWS/Assets/Scripts/RemoteMeshSource.cs
+++ CLAWS/Assets/Scripts/RemoteMeshSource.cs
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
-
+/*
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -229,3 +229,4 @@ namespace HoloToolkit.Unity
 #endif
     }
 }
+*/
