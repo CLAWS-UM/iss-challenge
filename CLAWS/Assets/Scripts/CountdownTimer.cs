d511b9ead851a6d1bf9200d25cb240ceba58aadc
cesarmu <cesarmu@umich.edu>
2019-04-06 22:38:30 -0400

    edit timer

--- CLAWS/Assets/Scripts/CountdownTimer.cs
+++ CLAWS/Assets/Scripts/CountdownTimer.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CountdownTimer : MonoBehaviour {
+public class CountdownTimer : MonoBehaviour
+{
     float currentTime;
     float startTime = 0f;
-    float endTime = 3f;
-    float warningTime = 1f; // last "warningTime" seconds of the task
 
     [SerializeField] Text timer;
 
@@ -19,24 +18,7 @@ public class CountdownTimer : MonoBehaviour {
 
     void Update()
     {
-        if (currentTime <= endTime)
-        {
-            currentTime += Time.deltaTime;
-            // display with warning color
-            if (currentTime >= (endTime - warningTime))
-            {
-                timer.color = Color.red;
-                timer.text = currentTime.ToString("0.0");
-            }
-            else
-            {
-                timer.color = Color.black;
-                timer.text = currentTime.ToString("0.0");
-            }
-        }
-        else
-        {
-            timer.text = "end";
-        }
+        currentTime += Time.deltaTime;
+        timer.text = currentTime.ToString("0.0");
     }
 }
