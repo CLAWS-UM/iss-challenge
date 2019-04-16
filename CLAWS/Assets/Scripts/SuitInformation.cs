4ff0b2570cd449c8a7e770c1a0288a1287e95267
Emily Rassel <emrassel@umich.edu>
2019-04-16 13:42:58 -0400

    New IPs for Sahil's hotspot

--- CLAWS/Assets/Scripts/SuitInformation.cs
+++ CLAWS/Assets/Scripts/SuitInformation.cs
@@ -60,8 +60,8 @@ namespace suitInfo
         public Text warningText;
 
         public int TaskFontSize = 15;
-        public string suitURL = "http://50.249.67.137:3000/api/suit/recent";
-        public string warningURL = "http://50.249.67.137:3000/api/suitswitch/recent";
+        public string suitURL = "http://192.168.43.148:3000/api/suit/recent";
+        public string warningURL = "http://192.168.43.148:3000/api/suitswitch/recent";
 
         // public GameObject textObject;
 
